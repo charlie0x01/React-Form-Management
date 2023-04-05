2fe4bc5988eb63c853a950f099d21d253985d9ee
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-04-06 00:37:07 +0500

    ✨Feature: added input validation and visual feedback using formik

--- react-formik-demo/src/components/TaskForm.jsx
+++ react-formik-demo/src/components/TaskForm.jsx
@@ -10,12 +10,25 @@ const onSubmit = (values) => {
   console.log("Form Submit: ", values);
 };
 
+const validate = (values) => {
+  let errors = {};
+
+  // validate input values
+  if (!values.name) errors.name = "Required";
+  if (values.name.length > 15) errors.name = "Must be 15 characters or less";
+
+  if (!values.status) errors.status = "Required";
+
+  return errors;
+};
+
 const TaskForm = () => {
   const formik = useFormik({
     initialValues: initValues,
     onSubmit: onSubmit,
+    validate: validate,
   });
-
+  console.log("Errors: ", formik.errors);
   return (
     <div className="is-flex is-justify-content-center">
       <form style={{ width: 350 }} onSubmit={formik.handleSubmit}>
@@ -31,7 +44,11 @@ const TaskForm = () => {
               placeholder="Text input"
               value={formik.values.name}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="error">{formik.errors.name}</div>
+            ) : null}
           </div>
         </div>
         <div className="field">
@@ -42,11 +59,15 @@ const TaskForm = () => {
             <select
               name="status"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.status}
             >
               <option>In Progress</option>
               <option>Completed</option>
             </select>
+            {formik.touched.status && formik.errors.status ? (
+              <div className="error">{formik.errors.status}</div>
+            ) : null}
           </div>
         </div>
         <button type="submit" className="button is-primary">
